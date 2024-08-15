5dfdbdaa53a7e8d5087918450f844b927c53f0c1
sushanth1234reddy <svreddy6889@gmail.com>
2024-08-15 23:37:41 +0530

    push3

---
+++ coffee.js
@@ -0,0 +1,22 @@
+// 1. Write a program to determine the type of coffee based on the user's selection:
+//     - 1: Espresso
+//     - 2: Cappuccino
+//     - 3: Latte
+//     - 4: Mocha
+let coffee="Cappuccino";
+switch(coffee){
+    case "Expresso":
+        console.log("User likes Expresso");
+        break;
+    case "Cappuccino":
+        console.log("User likes Cappuccino");
+        break;
+    case "Latte":
+        console.log("User likes Latte");
+        break;
+    case "Mocha":
+        console.log("User likes Mocha");
+        break;
+    default:
+        console.log("User does not like coffee");    
+}
---
+++ factorial.js
@@ -0,0 +1,8 @@
+let n=6;
+let num=n;
+let fact=1;
+while(num>0){
+    fact=fact*num;
+    num=num-1;
+}
+console.log(`Factorial ${n} is ${fact}`)
